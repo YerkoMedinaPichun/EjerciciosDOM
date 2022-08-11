71e69e7daa645d878321ec157195229219c69b36
YerkoMedinaPichun <yrk.27.medina@gmail.com>
2022-08-10 22:37:42 -0400

    Creando Enlace Otros Dinámicamente

--- dom/menu_hamburguesa.js
+++ dom/menu_hamburguesa.js
@@ -5,40 +5,42 @@
 //contenedor-btn-menu = panelBtn, menu = panel
 export default function funcionesMenuHamburguer() {
   const d = document;
-  configuracionInicial(d, "contenedor-btn-menu", "menu");
+
   hamburguerMenu(d, "btn-menu", "menu", "contenedor-enlace__enlace");
-  resize(d, "contenedor-btn-menu", "btn-menu", "menu");
-  // detectarAnchoPantalla(d, "contenedor-btn-menu", "btn-menu", "menu");
 }
 
-function hamburguerMenu(d, btnMenu, menu, menuLink) {
-  d.addEventListener("click", (e) => {
-    if (window.innerWidth < 768) {
-      if (e.target.matches(`#${btnMenu}`)) {
-        d.getElementById(btnMenu).classList.toggle("icono--encendido");
-        d.getElementById(menu).classList.toggle("menu--encendido");
-      }
-
-      if (e.target.matches(`.${menuLink}`)) {
-        d.getElementById(btnMenu).classList.remove("icono--encendido");
-        d.getElementById(menu).classList.remove("menu--encendido");
-      }
-      // if (e.target.matches(".contenedor-enlace__enlace")) {
-      //   d.getElementById(btnMenu).classList.remove("icono--encendido");
-      //   d.getElementById(menu).classList.remove("menu--encendido");
-      // }
-    }
-  });
-}
-
-function resize(d, contenedorBtnMenu, btnMenu, menu) {
+export function navAdaptable(d, w, mq, contenedorBtnMenu, btnMenu, menu) {
+  let breakpoint = w.matchMedia(mq);
   const $submenu = d.querySelectorAll(
     "#sub-menu .contenedor-enlace[data-submenu='1']"
   );
   const $otros = d.getElementById("otros");
   const $menu = d.getElementById("menu");
-  window.addEventListener("resize", (e) => {
-    if (window.innerWidth >= 768) {
+  /*----------------------------------------------*/
+  //Creando enlace Otros con
+  const $li = d.createElement("LI");
+  const $a = d.createElement("A");
+  const $ul = d.createElement("UL");
+  const $li_enlaces = d.querySelectorAll(`li[data-submenu="1"]`);
+  $li.setAttribute("id", "otros");
+  $li.classList.add("contenedor-enlace");
+
+  $a.classList.add("contenedor-enlace__enlace");
+  $a.textContent = "Otros";
+
+  $ul.setAttribute("id", "sub-menu");
+  $ul.classList.add("sub-menu");
+  $ul.classList.add("hidden");
+  /*------------------------------------------- */
+  const nav = (e) => {
+    if (e.matches) {
+      console.log("desktop");
+      if (d.getElementById("otros") !== null) {
+        $menu.removeChild($otros);
+        for (let i = 0; i < $submenu.length; i++) {
+          $menu.insertAdjacentElement("beforeend", $submenu[i]);
+        }
+      }
       /*
         Si el dispositivo es igual o mayor a 768px entonces agregamos la clase "ocultar"
       */
@@ -52,20 +54,37 @@ function resize(d, contenedorBtnMenu, btnMenu, menu) {
       if (!d.getElementById(menu).classList.contains("menu--encendido")) {
         d.getElementById(menu).classList.add("menu--encendido");
       }
+      // <li id="otros" class="contenedor-enlace"><a class="contenedor-enlace__enlace">Otros</a>
+      //   <ul id="sub-menu" class="sub-menu ">
 
-      //-------------
-      // console.log($submenu);
-      // if (d.querySelectorAll("#menu > .contenedor-enlace[data-submenu='1']")) {
-      //   for (let i = 0; i < $submenu.length; i++) {
-      //     $menu.removeChild($submenu[i]);
-      //   }
-      //   $menu.insertAdjacentElement("beforeend", $otros);
-      // }
-    }
+      //   </ul>
+      // </li>
+      if (!d.getElementById("otros")) {
+        $li_enlaces.forEach((enlace) => {
+          $ul.appendChild(enlace);
+        });
 
-    if (window.innerWidth < 768) {
+        $li.appendChild($a);
+        $li.appendChild($ul);
+
+        d.getElementById("menu").appendChild($li);
+        // console.log($li);
+        // console.log($li_enlaces);
+      }
+    } else {
+      console.log("mobile");
+      if (!d.getElementById(contenedorBtnMenu).classList.contains("ocultar")) {
+        d.getElementById(contenedorBtnMenu).classList.add("ocultar");
+      }
+
       /*
-        Si el dispositivo es igual o mayor a 768px entonces agregamos la clase "ocultar"
+        Si el dispositivo es menor a 768px y no posee la clase "menu--encendido", entonces se la agregamos
+      */
+      if (!d.getElementById(menu).classList.contains("menu--encendido")) {
+        d.getElementById(menu).classList.add("menu--encendido");
+      }
+      /*
+        Si el dispositivo es menor a 768px entonces quitamos la clase "ocultar"
       */
       if (d.getElementById(contenedorBtnMenu).classList.contains("ocultar")) {
         d.getElementById(contenedorBtnMenu).classList.remove("ocultar");
@@ -89,214 +108,31 @@ function resize(d, contenedorBtnMenu, btnMenu, menu) {
           d.getElementById(btnMenu).classList.add("icono--encendido");
         }
       }
+      console.log(d.getElementById("otros"));
+      if (d.getElementById("otros")) {
+        d.getElementById("menu").removeChild(d.getElementById("otros"));
+      }
+      $li_enlaces.forEach((enlace) => {
+        d.getElementById("menu").appendChild(enlace);
+      });
+    }
+  };
+  breakpoint.addEventListener("change", nav);
+  nav(breakpoint);
+}
 
-      // if (d.getElementById("otros") !== null) {
-      //   // console.log($menu);
-      //   // console.log($submenu.length);
-      //   // console.log($otros);
-      //   $menu.removeChild($otros);
-      //   for (let i = 0; i < $submenu.length; i++) {
-      //     $menu.insertAdjacentElement("beforeend", $submenu[i]);
-      //   }
-      // }
+function hamburguerMenu(d, btnMenu, menu, menuLink) {
+  d.addEventListener("click", (e) => {
+    if (window.innerWidth < 768) {
+      if (e.target.matches(`#${btnMenu}`)) {
+        d.getElementById(btnMenu).classList.toggle("icono--encendido");
+        d.getElementById(menu).classList.toggle("menu--encendido");
+      }
+
+      if (e.target.matches(`.${menuLink}`)) {
+        d.getElementById(btnMenu).classList.remove("icono--encendido");
+        d.getElementById(menu).classList.remove("menu--encendido");
+      }
     }
   });
 }
-
-function configuracionInicial(d, contenedorBtnMenu, menu) {
-  const $submenu = d.querySelectorAll(
-    "#sub-menu .contenedor-enlace[data-submenu='1']"
-  );
-  const $otros = d.getElementById("otros");
-  const $menu = d.getElementById("menu");
-  if (window.innerWidth >= 768) {
-    if (!d.getElementById(contenedorBtnMenu).classList.contains("ocultar")) {
-      d.getElementById(contenedorBtnMenu).classList.add("ocultar");
-    }
-
-    /*
-        Si el dispositivo es mayor a 768px y no posee la clase "menu--encendido", entonces se la agregamos
-      */
-    if (!d.getElementById(menu).classList.contains("menu--encendido")) {
-      d.getElementById(menu).classList.add("menu--encendido");
-    }
-    if (!d.getElementById("otros")) {
-    }
-  }
-  if (window.innerWidth < 768) {
-    if (d.getElementById("otros") !== null) {
-      // console.log($menu);
-      // console.log($submenu.length);
-      // console.log($otros);
-      $menu.removeChild($otros);
-      for (let i = 0; i < $submenu.length; i++) {
-        $menu.insertAdjacentElement("beforeend", $submenu[i]);
-      }
-    }
-  }
-}
-
-//------------------------------------------------------------------------
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const $anchoPantalla = window.innerWidth,
-//     $contenedorBtnMenu = document.getElementById("contenedor-btn-menu"),
-//     $botonMenu = document.getElementById("btn-menu"),
-//     $menu = document.getElementById("menu"),
-//     $main = document.querySelector(".main");
-
-//   cargarConfiguracionInicial($anchoPantalla, $contenedorBtnMenu, $menu);
-//   detectarAnchoPantalla($contenedorBtnMenu, $menu);
-//   menuResponsive($botonMenu, $menu);
-//   // menuSticky($contenedorBtnMenu, $menu, $main);
-// });
-
-// function cargarConfiguracionInicial(anchoPantalla, contenedorBtnMenu, menu) {
-//   //-----------------------------------------------------
-//   // const $anchoPantalla = window.innerWidth,
-//   //   $contenedorBtnMenu = document.getElementById("contenedor-btn-menu"),
-//   //   // $botonMenu = document.getElementById("btn-menu"),
-//   //   $menu = document.getElementById("menu");
-
-//   // if ($anchoPantalla >= 768) {
-//   //   if (!$contenedorBtnMenu.classList.contains("ocultar")) {
-//   //     $contenedorBtnMenu.classList.add("ocultar");
-//   //   }
-//   //   if ($menu.classList.contains("menu--apagado")) {
-//   //     $menu.classList.add("menu--encendido");
-//   //     $menu.classList.remove("menu--apagado");
-//   //   }
-//   // } else {
-//   // }
-//   //-----------------------------------------------------
-
-//   if (anchoPantalla >= 768) {
-//     if (!contenedorBtnMenu.classList.contains("ocultar")) {
-//       contenedorBtnMenu.classList.add("ocultar");
-//     }
-//     if (menu.classList.contains("menu--apagado")) {
-//       menu.classList.add("menu--encendido");
-//       menu.classList.remove("menu--apagado");
-//     }
-//     menu.style.top = "0";
-//     menu.style.position = "relative";
-//   } else {
-//   }
-// }
-
-// function detectarAnchoPantalla(contenedorBtnMenu, menu) {
-//   // const $contenedorBtnMenu = document.getElementById("contenedor-btn-menu"),
-//   //   $menu = document.getElementById("menu");
-
-//   // window.addEventListener("resize", (e) => {
-//   //   if (window.innerWidth >= 768) {
-//   //     if (!$contenedorBtnMenu.classList.contains("ocultar")) {
-//   //       $contenedorBtnMenu.classList.add("ocultar");
-//   //       $menu.classList.add("menu--encendido");
-//   //       $menu.classList.remove("menu--apagado");
-//   //     }
-//   //   } else {
-//   //     $contenedorBtnMenu.classList.remove("ocultar");
-//   //     $menu.classList.add("menu--apagado");
-//   //     $menu.classList.remove("menu--encendido");
-//   //   }
-//   // });
-
-//   window.addEventListener("resize", (e) => {
-//     if (window.innerWidth >= 768) {
-//       if (!contenedorBtnMenu.classList.contains("ocultar")) {
-//         contenedorBtnMenu.classList.add("ocultar");
-//         menu.classList.add("menu--encendido");
-//         menu.classList.remove("menu--apagado");
-//       }
-//     } else {
-//       contenedorBtnMenu.classList.remove("ocultar");
-//       menu.classList.add("menu--apagado");
-//       menu.classList.remove("menu--encendido");
-//     }
-//   });
-// }
-
-//------------------------------------------------------------------------
-
-// function menuResponsive(botonMenu, menu) {
-//   //----------------------------------------------------------------------
-//   // const $botonMenu = document.getElementById("btn-menu"),
-//   //   $menu = document.getElementById("menu");
-
-//   // $botonMenu.addEventListener("click", () => {
-//   //   $botonMenu.classList.toggle("contenedor-icono__icono--encendido");
-//   //   $botonMenu.classList.toggle("contenedor-icono__icono--apagado");
-//   //   $menu.classList.toggle("menu--encendido");
-//   //   $menu.classList.toggle("menu--apagado");
-//   // });
-//   //----------------------------------------------------------------------
-
-//   botonMenu.addEventListener("click", () => {
-//     botonMenu.classList.toggle("contenedor-icono__icono--encendido");
-//     botonMenu.classList.toggle("contenedor-icono__icono--apagado");
-//     menu.classList.toggle("menu--encendido");
-//     menu.classList.toggle("menu--apagado");
-//   });
-// }
-
-//------------------------------------------------------------------------
-
-// function menuSticky(contenedorBtnMenu, menu, main) {
-//   //--------------------------------------------------------------------
-//   // const $anchoPantalla = window.innerWidth,
-//   //   $contenedorBtnMenu = document.getElementById("contenedor-btn-menu"),
-//   //   $menu = document.getElementById("menu"),
-//   //   $main = document.querySelector(".main");
-
-//   // window.addEventListener("scroll", (e) => {
-//   //   console.log(window.scrollY);
-//   //   if (window.scrollY >= 130) {
-//   //     // $contenedorBtnNav.style.position = "fixed";
-//   //     $contenedorBtnMenu.classList.add("contenedor-icono--fixed");
-//   //     $menu.style.position = "fixed";
-//   //     $main.style.paddingTop = "7rem";
-//   //   } else {
-//   //     // $contenedorBtnNav.style.position = "relative";
-//   //     $contenedorBtnMenu.classList.remove("contenedor-icono--fixed");
-//   //     $menu.style.position = "absolute";
-//   //     $main.style.paddingTop = "0rem";
-//   //   }
-//   // });
-//   //--------------------------------------------------------------------
-
-//   window.addEventListener("scroll", (e) => {
-//     if (window.innerWidth < 768) {
-//       if (window.scrollY >= 130) {
-//         // $contenedorBtnNav.style.position = "fixed";
-//         contenedorBtnMenu.style.position = "fixed";
-//         contenedorBtnMenu.style.top = "0";
-//         menu.style.position = "fixed";
-//         main.style.paddingTop = "7rem";
-//         /*
-//       position: fixed;
-//       top: 0;
-//       */
-//       } else {
-//         // $contenedorBtnNav.style.position = "relative";
-//         contenedorBtnMenu.style.position = "relative";
-
-//         menu.style.position = "absolute";
-//         main.style.paddingTop = "0rem";
-//       }
-//     } else {
-//       if (window.scrollY >= 85) {
-//         console.log(menu);
-//         menu.style.position = "fixed";
-//         menu.style.top = "0";
-//         main.style.paddingTop = "11rem";
-//       } else {
-//         menu.style.position = "relative";
-//         //menu.style.top = "0";
-//         main.style.paddingTop = "0rem";
-//       }
-//     }
-//   });
-// }
-
-//------------------------------------------------------------------------
--- index_dom.js
+++ index_dom.js
@@ -1,7 +1,9 @@
 import { cuentaRegresiva } from "./dom/cuenta_regresiva.js";
 import { darkMode } from "./dom/dark_mode.js";
 import { menuDesplegable } from "./dom/menu_desplegable.js";
-import funcionesMenuHamburguer from "./dom/menu_hamburguesa.js";
+import funcionesMenuHamburguer, {
+  navAdaptable,
+} from "./dom/menu_hamburguesa.js";
 import responsiveMedia from "./dom/objeto_responsive.js";
 import responsiveTester from "./dom/prueba_responsive.js";
 import { alarma, relojDigital } from "./dom/reloj_alarma.js";
@@ -13,6 +15,15 @@ const w = window;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   funcionesMenuHamburguer();
+  navAdaptable(
+    d,
+    w,
+    "(min-width: 768px)",
+    "contenedor-btn-menu",
+    "btn-menu",
+    "menu"
+  );
+
   relojDigital(d, "reloj", "iniciar-reloj", "detener-reloj");
   alarma(d, "alarma", "iniciar-alarma", "detener-alarma");
   funcionesEventosTeclado(d, "contenedor-lienzo");
@@ -36,46 +47,45 @@ d.addEventListener("DOMContentLoaded", (e) => {
     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3322.6396690403785!2d-70.62570378255616!3d-33.6146526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662d82ac5e49f1f%3A0xfd4622ee928b10c0!2sEstero%20Leonera%20480%2C%20Puente%20Alto%2C%208167445%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1659821913900!5m2!1ses-419!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   );
 
-  responsiveMedia(
-    d,
-    w,
-    "menu",
-    "(min-width: 768px)",
-    `
-      <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-1">Reloj y Alarma</a></li>
-      <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-2">Eventos Teclado</a></li>
-      <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-3">Cuenta Regresiva</a></li>
-      <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-4">Responsive con JS</a></li>
-      
+  // responsiveMedia(
+  //   d,
+  //   w,
+  //   "menu",
+  //   "(min-width: 768px)",
+  //   `
+  //     <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-1">Reloj y Alarma</a></li>
+  //     <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-2">Eventos Teclado</a></li>
+  //     <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-3">Cuenta Regresiva</a></li>
+  //     <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-4">Responsive con JS</a></li>
 
-    <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-5">Responsive Tester</a></li><li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-6">Sección
-              6</a></li><li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-8">Sección
-              8</a></li><li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-9">Sección
-              9</a></li><li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-10">Sección
-              10</a></li>`,
-    `
-      <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-1">Reloj y Alarma</a></li>
-      <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-2">Eventos Teclado</a></li>
-      <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-3">Cuenta Regresiva</a></li>
-      <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-4">Responsive con JS</a></li>
-      <li id="otros" class="contenedor-enlace"><a class="contenedor-enlace__enlace">Otros</a>
-        <ul id="sub-menu" class="sub-menu hidden">
-          <!--  -->
-          <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-5">Responsive Tester</a></li>
-          <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-6">Sección
-              6</a></li>
-          <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-8">Sección
-              8</a></li>
-          <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-9">Sección
-              9</a></li>
-          <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-10">Sección
-              10</a></li>
+  //   <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-5">Responsive Tester</a></li><li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-6">Sección
+  //             6</a></li><li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-8">Sección
+  //             8</a></li><li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-9">Sección
+  //             9</a></li><li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-10">Sección
+  //             10</a></li>`,
+  //   `
+  //     <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-1">Reloj y Alarma</a></li>
+  //     <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-2">Eventos Teclado</a></li>
+  //     <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-3">Cuenta Regresiva</a></li>
+  //     <li class="contenedor-enlace"><a class="contenedor-enlace__enlace" href="#seccion-4">Responsive con JS</a></li>
+  //     <li id="otros" class="contenedor-enlace"><a class="contenedor-enlace__enlace">Otros</a>
+  //       <ul id="sub-menu" class="sub-menu hidden">
+  //         <!--  -->
+  //         <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-5">Responsive Tester</a></li>
+  //         <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-6">Sección
+  //             6</a></li>
+  //         <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-8">Sección
+  //             8</a></li>
+  //         <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-9">Sección
+  //             9</a></li>
+  //         <li class="contenedor-enlace" data-submenu="1"><a class="contenedor-enlace__enlace" href="#seccion-10">Sección
+  //             10</a></li>
 
-        </ul>
-      </li>
+  //       </ul>
+  //     </li>
 
-    `
-  );
+  //   `
+  // );
 
   responsiveTester(d, "responsive-tester");
 });
