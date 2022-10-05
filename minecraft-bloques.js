ea9dc1c7520d7ea8457cb95c107a65fc3a549d6b
YerkoMedinaPichun <yrk.27.medina@gmail.com>
2022-10-05 00:51:49 -0300

    CRUD bloques minecraft con Fetch

--- minecraft-bloques.js
+++ minecraft-bloques.js
@@ -10,100 +10,196 @@ const $templateOptionTag = d.getElementById("option-tag").content,
 
 let options;
 
-const ajax = (options) => {
-  let { url, method, success, error, data } = options;
-  const xhr = new XMLHttpRequest();
+// const ajax = (options) => {
+//   let { url, method, success, error, data } = options;
+//   const xhr = new XMLHttpRequest();
 
-  xhr.addEventListener("readystatechange", (e) => {
-    if (xhr.readyState !== 4) return;
-    if (xhr.status >= 200 && xhr.status < 300) {
-      let json = JSON.parse(xhr.responseText);
-      options = json;
-      success(json);
-    } else {
-      let message = xhr.statusText || "Ocurrió un error";
-      error(`Error ${xhr.status}: ${message}`);
-    }
-  });
+//   xhr.addEventListener("readystatechange", (e) => {
+//     if (xhr.readyState !== 4) return;
+//     if (xhr.status >= 200 && xhr.status < 300) {
+//       let json = JSON.parse(xhr.responseText);
+//       options = json;
+//       success(json);
+//     } else {
+//       let message = xhr.statusText || "Ocurrió un error";
+//       error(`Error ${xhr.status}: ${message}`);
+//     }
+//   });
 
-  xhr.open(method || "GET", url);
-  xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
-  xhr.send(JSON.stringify(data));
+//   xhr.open(method || "GET", url);
+//   xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+//   xhr.send(JSON.stringify(data));
+// };
+
+const getAllOptionsFetch = async () => {
+  try {
+    let res = await fetch("http://localhost:5555/tags"),
+      json = await res.json();
+
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
+    json.forEach((el) => {
+      $template.querySelector(".name-option").textContent = el.name;
+
+      $template.querySelector(".edit-option").dataset.id = el.id;
+      $template.querySelector(".edit-option").dataset.name = el.name;
+
+      $template.querySelector(".delete-option").dataset.id = el.id;
+      $template.querySelector(".delete-option").dataset.name = el.name;
+
+      // =======================
+
+      $templateOptionTag.querySelector(".option-name").textContent = el.name;
+
+      $templateOptionTag.querySelector(".option-name").dataset.id = el.id;
+
+      $templateOptionTag.querySelector(".option-name").dataset.name = el.name;
+
+      let $clone = d.importNode($template, true);
+      $fragment.appendChild($clone);
+
+      let $cloneOptionTag = d.importNode($templateOptionTag, true);
+      $fragmentOptionTag.appendChild($cloneOptionTag);
+    });
+    $table.querySelector("tbody").appendChild($fragment);
+    d.querySelector(".crud-form-block").appendChild($fragmentOptionTag);
+  } catch (error) {
+    let message = error.statusText || "Ocurrió un error";
+    $table.insertAdjacentHTML(
+      "afterend",
+      `<p><b>Error ${error.status}: ${message}</b></p>`
+    );
+  }
 };
 
-const getAllOptions = () => {
-  ajax({
-    url: "http://localhost:5555/tags",
-    success: (res) => {
-      console.log(res);
-      res.forEach((el) => {
-        $template.querySelector(".name-option").textContent = el.name;
+// const getAllOptions = () => {
+//   ajax({
+//     url: "http://localhost:5555/tags",
+//     success: (res) => {
+//       console.log(res);
+//       res.forEach((el) => {
+//         $template.querySelector(".name-option").textContent = el.name;
 
-        $template.querySelector(".edit-option").dataset.id = el.id;
-        $template.querySelector(".edit-option").dataset.name = el.name;
+//         $template.querySelector(".edit-option").dataset.id = el.id;
+//         $template.querySelector(".edit-option").dataset.name = el.name;
 
-        $template.querySelector(".delete-option").dataset.id = el.id;
-        $template.querySelector(".delete-option").dataset.name = el.name;
+//         $template.querySelector(".delete-option").dataset.id = el.id;
+//         $template.querySelector(".delete-option").dataset.name = el.name;
 
-        // =======================
+//         // =======================
 
-        $templateOptionTag.querySelector(".option-name").textContent = el.name;
+//         $templateOptionTag.querySelector(".option-name").textContent = el.name;
 
-        $templateOptionTag.querySelector(".option-name").dataset.id = el.id;
+//         $templateOptionTag.querySelector(".option-name").dataset.id = el.id;
 
-        $templateOptionTag.querySelector(".option-name").dataset.name = el.name;
+//         $templateOptionTag.querySelector(".option-name").dataset.name = el.name;
 
-        let $clone = d.importNode($template, true);
-        $fragment.appendChild($clone);
+//         let $clone = d.importNode($template, true);
+//         $fragment.appendChild($clone);
 
-        let $cloneOptionTag = d.importNode($templateOptionTag, true);
-        $fragmentOptionTag.appendChild($cloneOptionTag);
-      });
-      $table.querySelector("tbody").appendChild($fragment);
-      d.querySelector(".crud-form-block").appendChild($fragmentOptionTag);
-    },
-    error: (e) => {
-      console.log(e);
-      $table.insertAdjacentHTML("afterend", `<p><b>${e}</b></p>`);
-    },
-  });
-};
+//         let $cloneOptionTag = d.importNode($templateOptionTag, true);
+//         $fragmentOptionTag.appendChild($cloneOptionTag);
+//       });
+//       $table.querySelector("tbody").appendChild($fragment);
+//       d.querySelector(".crud-form-block").appendChild($fragmentOptionTag);
+//     },
+//     error: (e) => {
+//       console.log(e);
+//       $table.insertAdjacentHTML("afterend", `<p><b>${e}</b></p>`);
+//     },
+//   });
+// };
 
-d.addEventListener("DOMContentLoaded", getAllOptions);
+d.addEventListener("DOMContentLoaded", getAllOptionsFetch);
 
-d.addEventListener("submit", (e) => {
+d.addEventListener("submit", async (e) => {
   if (e.target === $formOptions) {
     e.preventDefault();
-    if (!e.target.id.value) {
-      //CREATE - POST
 
-      ajax({
-        url: "http://localhost:5555/tags",
-        method: "POST",
-        success: (res) => location.reload(),
-        error: (e) =>
-          $formOptions.insertAdjacentHTML("afterend", `<p><b>${e}</b></p>`),
-        data: {
-          name: e.target.name.value,
-        },
-      });
+    if (!e.target.id.value) {
+      try {
+        let options = {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify({
+              name: e.target.name.value,
+            }),
+          },
+          res = await fetch("http://localhost:5555/tags", options),
+          json = await res.json();
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (error) {
+        let message = error.statusText || "Ocurrió un error";
+        $formOptions.insertAdjacentHTML(
+          "afterend",
+          `<p><b>Error ${error.status} : ${message}</b></p>`
+        );
+      }
     } else {
-      //UPDATE - PUT
-      ajax({
-        url: `http://localhost:5555/tags/${e.target.id.value}`,
-        method: "PUT",
-        success: (res) => location.reload(),
-        error: (e) =>
-          $formOptions.insertAdjacentHTML("afterend", `<p><b>${e}</b></p>`),
-        data: {
-          name: e.target.name.value,
-        },
-      });
+      try {
+        let options = {
+            method: "PUT",
+            headers: {
+              "Content-type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify({
+              name: e.target.name.value,
+            }),
+          },
+          res = await fetch(
+            `http://localhost:5555/tags/${e.target.id.value}`,
+            options
+          ),
+          json = await res.json();
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (error) {
+        let message = error.statusText || "Ocurrió un error";
+        $formOptions.insertAdjacentHTML(
+          "afterend",
+          `<p><b>Error ${error.status} : ${message}</b></p>`
+        );
+      }
     }
   }
 });
 
-d.addEventListener("click", (e) => {
+// d.addEventListener("submit", (e) => {
+//   if (e.target === $formOptions) {
+//     e.preventDefault();
+//     if (!e.target.id.value) {
+//       //CREATE - POST
+
+//       ajax({
+//         url: "http://localhost:5555/tags",
+//         method: "POST",
+//         success: (res) => location.reload(),
+//         error: (e) =>
+//           $formOptions.insertAdjacentHTML("afterend", `<p><b>${e}</b></p>`),
+//         data: {
+//           name: e.target.name.value,
+//         },
+//       });
+//     } else {
+//       //UPDATE - PUT
+//       ajax({
+//         url: `http://localhost:5555/tags/${e.target.id.value}`,
+//         method: "PUT",
+//         success: (res) => location.reload(),
+//         error: (e) =>
+//           $formOptions.insertAdjacentHTML("afterend", `<p><b>${e}</b></p>`),
+//         data: {
+//           name: e.target.name.value,
+//         },
+//       });
+//     }
+//   }
+// });
+
+d.addEventListener("click", async (e) => {
   if (e.target.matches(".edit-option")) {
     $titleOption.textContent = "Editar Opción";
     $formOptions.name.value = e.target.dataset.name;
@@ -115,12 +211,46 @@ d.addEventListener("click", (e) => {
     );
 
     if (isDelete) {
-      ajax({
-        url: `http://localhost:5555/tags/${e.target.dataset.id}`,
-        method: "DELETE",
-        success: (res) => location.reload(),
-        error: (e) => alert(e),
-      });
+      try {
+        let options = {
+            method: "DELETE",
+            headers: {
+              "Content-type": "application/json; charset=utf-8",
+            },
+          },
+          res = await fetch(
+            `http://localhost:5555/tags/${e.target.dataset.id}`,
+            options
+          ),
+          json = await res.json();
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (error) {
+        let message = error.statusText || "Ocurrió un error";
+        alert(`Error ${error.status}: ${message}`);
+      }
     }
   }
 });
+
+// d.addEventListener("click", (e) => {
+//   if (e.target.matches(".edit-option")) {
+//     $titleOption.textContent = "Editar Opción";
+//     $formOptions.name.value = e.target.dataset.name;
+//     $formOptions.id.value = e.target.dataset.id;
+//   }
+//   if (e.target.matches(".delete-option")) {
+//     let isDelete = confirm(
+//       `¿Estás seguro de eliminar el id "${e.target.dataset.id}" de nombre "${e.target.dataset.name}"?`
+//     );
+
+//     if (isDelete) {
+//       ajax({
+//         url: `http://localhost:5555/tags/${e.target.dataset.id}`,
+//         method: "DELETE",
+//         success: (res) => location.reload(),
+//         error: (e) => alert(e),
+//       });
+//     }
+//   }
+// });
